8c3174c11c28b764db9bebd92c4504103c572c2f
jomsq <jomalynquebral@gmail.com>
2024-07-04 21:17:25 +0800

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+ 
+  
+  
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+}
